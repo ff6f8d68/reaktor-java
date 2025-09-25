b365f28ccea0d3e3333836fc27edac960d0092a6
ff6f8d68 <atom20003113@Mac-mini-von-atomdev.local>
2025-09-25 15:16:04 +0200

    fixed API

--- src/main/java/maven/hexagon/reaktorjava/Reaktor.java
+++ src/main/java/maven/hexagon/reaktorjava/Reaktor.java
@@ -7,22 +7,42 @@ import java.util.Random;
 import java.util.Set;
 
 import com.google.gson.Gson;
+
 public class Reaktor {
     private ReactionEngine engine = new ReactionEngine();
 
-    public Atom createAtom(int protons, int neutrons, int electrons, String elementName,
-                           boolean explosive, double explosionStrength,
-                           boolean flammable, double fireStrength,
-                           String state, String color) {
+    /**
+     * Creates a new atom and returns it as an Object.
+     */
+    public Object createAtom(int protons, int neutrons, int electrons, String elementName,
+                             boolean explosive, double explosionStrength,
+                             boolean flammable, double fireStrength,
+                             String state, String color) {
         return new Atom(protons, neutrons, electrons, elementName,
                 explosive, explosionStrength, flammable, fireStrength, state, color);
     }
 
-    public List<Atom> react(List<Atom> atoms) {
-        return engine.react(atoms);
+    /**
+     * Reacts a list of atoms and returns a list of reaction products as Objects.
+     */
+    public List<Object> react(List<Object> atoms) {
+        List<Atom> atomList = new ArrayList<>();
+        for (Object o : atoms) {
+            if (o instanceof Atom a) {
+                atomList.add(a);
+            } else {
+                throw new IllegalArgumentException("Invalid object type: " + o.getClass());
+            }
+        }
+
+        List<Atom> products = engine.react(atomList);
+        return new ArrayList<>(products);
     }
 
-    public String toJson(List<Atom> atoms) {
+    /**
+     * Serializes a list of atoms to JSON.
+     */
+    public String toJson(List<Object> atoms) {
         return new Gson().toJson(atoms);
     }
 }
